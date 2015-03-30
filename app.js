7e80ccc3fa593982c9ff5a0b055dbe8fa2ae08ce
Carlos Bolanos <mail@carlos-bolanos.com>
2015-03-30 16:35:25 -0600

    consolidate and swig

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
-//var MongoClient = require("mongodb").MongoClient;
-var express = require('express');
+var express = require('express'),
+    app = express(),
+    cons = require('consolidate'); // Templating library adapter for Express
 
-var app = express();
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res){
-	res.send("HelloWorld");	
+    res.render('hello', { name : 'SWIG' });
 });
 
 app.get('*', function(req, res){
-	res.send("Page not found", 404);	
+    res.send('Page Not Found', 404);
 });
 
 app.listen(8080);
-console.log("server started");
-
-
-/*
-MongoClient.connect('mongodb://localhost:27017/test', function(err, db){
-
-	if(err) throw err;
-
-	db.collection('things').findOne({}, function(err, doc){
-		if(err) throw err;
-		
-		console.dir(doc);
-
-		db.close();
-	});
-
-	//console.dir("called findOne");
-});
-*/
+console.log('Express server started on port 8080');

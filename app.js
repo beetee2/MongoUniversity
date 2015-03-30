368525ab5f14d39e488468d6ff89639346df64b3
unknown <bocarlos@BOCARLOS1.americas.hpqcorp.net>
2015-03-30 15:32:37 -0600

    first connection/query to mongodb

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-var express = require("express");
-var cons = require("consolidate");
-var mongo = require("mongodb");
+var MongoClient = require("mongodb").MongoClient;
+
+console.log('not started')
+
+MongoClient.connect('mongodb://localhost:27017/test', function(err, db){
+	console.log('connected');
+
+	if(err) throw err;
+
+	db.collection('things').findOne({}, function(err, doc){
+		if(err) throw err;
+		
+		console.dir(doc);
+
+		db.close();
+	});
+
+	console.dir("called findOne");
+});
 

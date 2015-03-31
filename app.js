32690baf05ffb3a6295cdac2025e7742d2b3cf23
Carlos Bolanos <mail@carlos-bolanos.com>
2015-03-31 15:39:13 -0600

    homeworks

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express'),
 	cons = require('consolidate'),
     Db = require('mongodb').Db,
     MongoClient = require('mongodb').MongoClient,
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    crypto = require('crypto'),
+    assert = require('assert');
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
@@ -19,7 +21,7 @@ function errorHandler(err, req, res, next){
 	console.error(err.stack);
 	res.status(500);
 	
-	res.render(error_template, {error: err});
+	res.render('error_template', {error: err});
 }
 
 app.use(errorHandler);
@@ -34,35 +36,77 @@ app.use(errorHandler);
  res.render('hello', {name: name, getVar1: GetVar1, getVar2: GetVar2});
  });
 */
-app.route('/fruits')
-    .get(function(req,res, next){
-        res.render("fruitPicker", {fruits: ["apple","orange","banana","peach"]});
-    })
-    .post(function(req, res, next){
-        var favorite = req.body.fruit;
-        if(favorite == undefined)
-            next(Error('Please choose a fruit'));
-        res.send("Your favorite fruit is: " + favorite);
-    });
-
 /*
+* app.route('/fruits')
+ .get(function(req,res, next){
+ res.render("fruitPicker", {fruits: ["apple","orange","banana","peach"]});
+ })
+ .post(function(req, res, next){
+ var favorite = req.body.fruit;
+ if(favorite == undefined)
+ next(Error('Please choose a fruit'));
+ res.send("Your favorite fruit is: " + favorite);
+ });
+/*
+
+ */
+
 app.get('/', function(req, res){
 
-	Db.collection('things').findOne({}, function(err, doc){
-		if(err) throw err;
+    /*
+     Db.collection('hw1_1').findOne({}, function(err, doc){
+     if(err) throw err;
 
-		//console.dir(doc);
-		res.render('hello',  doc );
-	});
+     if (!doc) {
+     console.dir("No document found");
+     return db.close();
+     }
+
+     console.log(doc);
+
+     return Db.close();
+     });
+
+     Db.collection('hw1_2').findOne({}, function(err, doc){
+     if(err) throw err;
+
+     if (!doc) {
+     console.dir("No document found");
+     return db.close();
+     }
+     var algorithm = 'aes256';
+     var encrypted_message = '7013254dca77e2c913d18cf5b70e7bba';
+
+     var decipher = crypto.createDecipher(algorithm, doc['_id']);
+     var decrypted = decipher.update(encrypted_message, 'hex', 'utf8') + decipher.final('utf8');
+     console.log("Answer: " + decrypted);
+
+     return Db.close();
+     });
+    */
+
+    Db.collection('hw1_3').findOne({}, function(err, doc){
+        if(err) throw err;
+
+        if (!doc) {
+            console.dir("No document found");
+            return db.close();
+        }
+        var algorithm = 'aes256';
+        var encrypted_message = 'f36731a12e6130f0ed0bccbfd9bd6ebd';
+        var decipher = crypto.createDecipher(algorithm, doc['_id']);
+        var decrypted = decipher.update(encrypted_message, 'hex', 'utf8') + decipher.final('utf8');
+        return res.render('hello', { "name" : decrypted });
+    });
     
 });
-*/
+
 
 app.get('*', function(req, res){
     res.status(404).send('Page Not Found');
 });
 
-var url = "mongodb://localhost:27017/test";
+var url = "mongodb://localhost:27017/m101";
 
 MongoClient.connect(url, function (err, db){
 	if(err) throw err;

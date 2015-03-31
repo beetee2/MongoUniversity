dca042fe3436d5a7a71d3d72f94c67c8cd753f36
Carlos Bolanos <mail@carlos-bolanos.com>
2015-03-31 11:37:29 -0600

    MongoClient

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
 var express = require('express'),
-    app = express(),
-    cons = require('consolidate'); // Templating library adapter for Express
+	app = express(),
+	cons = require('consolidate'),
+    Db = require('mongodb').Db,
+    MongoClient = require('mongodb').MongoClient,
+	Server = require('mongodb').Server;
+
+
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
+
+
+
 app.get('/', function(req, res){
-    res.render('hello', { name : 'SWIG' });
+
+	Db.collection('things').findOne({}, function(err, doc){
+		if(err) throw err;
+
+		//console.dir(doc);
+		res.render('hello',  doc );
+	});
+    
 });
 
 app.get('*', function(req, res){
     res.send('Page Not Found', 404);
 });
 
-app.listen(8080);
-console.log('Express server started on port 8080');
+var url = "mongodb://localhost:27017/test";
+MongoClient.connect(url, function (err, db){
+	
+	if(err) throw err;
+
+    Db = db;
+
+	app.listen(8080);
+	console.log('Express server started on port 8080');
+});
+
+
